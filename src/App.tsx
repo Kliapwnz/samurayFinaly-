e3054c6e1026d9e95c761457154e8d18a759e7df
Evgeniy Klimovch <kliasanik@gmail.com>
2023-04-11 12:03:48 +0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import './App.css';
 
 function App() {
     return (
-        <div className="App">
-
+        <div>
+            <header>
+                <img src="https://img.freepik.com/free-vector/golden-bird-logo-design_1195-336.jpg?w=2000"/>
+            </header>
         </div>
     );
 }
