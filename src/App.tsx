bcb4fe08c0bba70e3160cced60cf8c56fcaca04f
Evgeniy Klimovch <kliasanik@gmail.com>
2023-04-11 13:27:41 +0300

    grids

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,11 @@ function App() {
                 <img src="https://img.freepik.com/free-vector/golden-bird-logo-design_1195-336.jpg?w=2000"/>
             </header>
             <nav className="nav">
-                <div>Profile</div>
-                <div>Messages</div>
-                <div>Profile</div>
+                <div><a>Profile</a></div>
+                <div><a>Messages</a></div>
+                <div><a>News</a></div>
+                <div><a>Music</a></div>
+                <div><a>Settings</a></div>
             </nav>
             <div className="content">Main Content</div>
         </div>
