61de855640fc45954fbd2197d36d9bc1360889cf
Evgeniy Klimovch <kliasanik@gmail.com>
2023-04-12 10:26:30 +0300

    ava + descript

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,13 @@ function App() {
                 <div><a>Settings</a></div>
             </nav>
             <div className="content">
-                <img src="https://static8.depositphotos.com/1021722/859/i/600/depositphotos_8599315-stock-photo-art-business-power.jpg"/>
+                <div>
+                    <img
+                        src="https://static8.depositphotos.com/1021722/859/i/600/depositphotos_8599315-stock-photo-art-business-power.jpg"/>
+                </div>
+                <div>
+                    ava + description
+                </div>
             </div>
         </div>
     );
