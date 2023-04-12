e54f515b49fe12adc4f9ed5ad692f85a28c96470
Evgeniy Klimovch <kliasanik@gmail.com>
2023-04-12 10:28:18 +0300

    posts

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,17 @@ function App() {
                 <div>
                     ava + description
                 </div>
+                <div>
+                    my posts
+                    <div>
+                        New Post
+                    </div>
+                    <div>
+                        <div>Post1</div>
+                        <div>Post2</div>
+                        <div>Post3</div>
+                    </div>
+                </div>
             </div>
         </div>
     );
