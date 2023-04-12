55ee3e9fffdcffe0162aa363678426c0f6323ca6
Evgeniy Klimovch <kliasanik@gmail.com>
2023-04-12 10:20:37 +0300

    main content

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,9 @@ function App() {
                 <div><a>Music</a></div>
                 <div><a>Settings</a></div>
             </nav>
-            <div className="content">Main Content</div>
+            <div className="content">
+                <img src="https://static8.depositphotos.com/1021722/859/i/600/depositphotos_8599315-stock-photo-art-business-power.jpg"/>
+            </div>
         </div>
     );
 }
