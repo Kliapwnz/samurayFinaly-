530085e8269ebdfab89b26ffb4eb4691ba566306
Evgeniy Klimovch <kliasanik@gmail.com>
2023-05-04 13:50:32 +0300

    Profile.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
     return (
         <div className="header">
             <img src="https://img.freepik.com/free-vector/golden-bird-logo-design_1195-336.jpg?w=2000"/>
-            </div>
+        </div>
     );
 };
 
