25d2596773e7c0add752a5d03675cdcf2c1197e1
Evgeniy Klimovch <kliasanik@gmail.com>
2023-05-10 11:43:47 +0300

    classes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import "./Navbar.module.css"
+import classes from "./Navbar.module.css"
 
 export const Navbar = () => {
     return (
-        <div className="nav">
+        <div className={classes.nav}>
             <div className="item"><a>Profile</a></div>
             <div className="item"><a>Messages</a></div>
             <div className="item"><a>News</a></div>
