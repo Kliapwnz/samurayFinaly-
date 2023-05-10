b6de3c3222e9e87ae4c0095d96859c2133fb73c9
Evgeniy Klimovch <kliasanik@gmail.com>
2023-05-10 11:51:54 +0300

    classes

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -16,7 +16,7 @@ export const Profile = () => {
                 <div>
                     New Post
                 </div>
-                <div>
+                <div className={s.item}>
                     <div>Post1</div>
                     <div>Post2</div>
                     <div>Post3</div>
