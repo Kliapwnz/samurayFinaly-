87edbd304c0a2d4c4f3961533581cae5227023fd
Evgeniy Klimovch <kliasanik@gmail.com>
2023-05-10 12:39:09 +0300

    span like

--- src/components/profile/myPosts/post/Post.tsx
+++ src/components/profile/myPosts/post/Post.tsx
@@ -7,6 +7,7 @@ export const Post = () => {
             <img
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrLF2dK9mac4u0-IbZBFiFoIyKku6oxrqDNA&usqp=CAU"/>
             Post1
+            <span>Like</span>
         </div>
     );
 };
