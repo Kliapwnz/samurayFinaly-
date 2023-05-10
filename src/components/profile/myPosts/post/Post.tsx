a1acabf0a2e581a489d6863be2662f7fdfa96a54
Evgeniy Klimovch <kliasanik@gmail.com>
2023-05-10 12:34:49 +0300

    Post.tsx

---
+++ src/components/profile/myPosts/post/Post.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import s from "../MyPosts.module.css";
+
+export const Post = () => {
+    return (
+
+        <div className={s.item}>
+            <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrLF2dK9mac4u0-IbZBFiFoIyKku6oxrqDNA&usqp=CAU"/>
+            Post1
+        </div>
+
+    );
+};
+
