afab9dd9d15b684ca5994aeef2ffbb9e8258a178
Evgeniy Klimovch <kliasanik@gmail.com>
2023-05-10 12:35:53 +0300

    Post.tsx

--- src/components/profile/myPosts/MyPosts.tsx
+++ src/components/profile/myPosts/MyPosts.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import s from "./MyPosts.module.css";
+import {Post} from "./post/Post";
 
 export const MyPosts = () => {
     return (
@@ -10,11 +11,7 @@ export const MyPosts = () => {
                 <button>Add Post</button>
             </div>
             <div className={s.posts}>
-                <div className={s.item}>
-                    <img
-                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrLF2dK9mac4u0-IbZBFiFoIyKku6oxrqDNA&usqp=CAU"/>
-                    Post1
-                </div>
+                <Post/>
                 <div className={s.item}>Post2</div>
                 <div className={s.item}>Post3</div>
             </div>
--- src/components/profile/myPosts/post/Post.tsx
+++ src/components/profile/myPosts/post/Post.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import s from "../MyPosts.module.css";
+import s from "./Post.module.css";
 
 export const Post = () => {
     return (
-
         <div className={s.item}>
             <img
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrLF2dK9mac4u0-IbZBFiFoIyKku6oxrqDNA&usqp=CAU"/>
             Post1
         </div>
-
     );
 };
 
