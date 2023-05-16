a57d5aa43253557b90e88b52a8a819ba3fa4b376
Evgeniy Klimovch <kliasanik@gmail.com>
2023-05-17 01:18:42 +0300

    active link

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,11 +6,21 @@ import {NavLink} from "react-router-dom";
 export const Navbar = () => {
     return (
         <div className={s.nav}>
-            <div className={s.item}><NavLink to="/profile" className = { navData => navData.isActive ? s.active : s.item }>Profile</NavLink></div>
-            <div className={`${s.item} ${s.active}`}><NavLink to="/dialogs" className = { navData => navData.isActive ? s.active : s.item }>Messages</NavLink></div>
-            <div className={s.item}><NavLink to="News" className = { navData => navData.isActive ? s.active : s.item }>News</NavLink></div>
-            <div className={s.item}><NavLink to="Music" className = { navData => navData.isActive ? s.active : s.item }>Music</NavLink></div>
-            <div className={s.item}><NavLink to="Settings" className = { navData => navData.isActive ? s.active : s.item }>Settings</NavLink></div>
+            <div className={s.item}><NavLink to="/profile"
+                                             className={navData => navData.isActive ? s.active : s.item}>Profile</NavLink>
+            </div>
+            <div className={`${s.item} ${s.active}`}><NavLink to="/dialogs"
+                                                              className={navData => navData.isActive ? s.active : s.item}>Messages</NavLink>
+            </div>
+            <div className={s.item}><NavLink to="News"
+                                             className={navData => navData.isActive ? s.active : s.item}>News</NavLink>
+            </div>
+            <div className={s.item}><NavLink to="Music"
+                                             className={navData => navData.isActive ? s.active : s.item}>Music</NavLink>
+            </div>
+            <div className={s.item}><NavLink to="Settings"
+                                             className={navData => navData.isActive ? s.active : s.item}>Settings</NavLink>
+            </div>
         </div>
     );
 };
