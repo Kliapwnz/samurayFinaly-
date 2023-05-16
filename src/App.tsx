1073021bb7b58c10aa7bf83398c4687da0be8048
Evgeniy Klimovch <kliasanik@gmail.com>
2023-05-17 01:02:37 +0300

    BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,24 @@ import {Header} from "./components/header/Header";
 import {Navbar} from "./components/navbar/Navbar";
 import {Profile} from "./components/profile/Profile";
 import {Dialogs} from "./components/dialogs/Dialogs";
-import {Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 
 function App() {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <Navbar/>
-            <Routes>
-            <div className="app-wrapper-content">
-                <Route path="/profile" element={<Profile/>}/>
-                <Route path="/dialogs" element={<Dialogs/>}/>
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <div className="app-wrapper-content">
+                    <Routes>
+                        <Route path="/profile" element={<Profile/>}/>
+                        <Route path="/dialogs" element={<Dialogs/>}/>
+                    </Routes>
+                </div>
             </div>
-            </Routes>
-            {/*<Profile/>*/}
-        </div>
+        </BrowserRouter>
+
     );
 }
 
