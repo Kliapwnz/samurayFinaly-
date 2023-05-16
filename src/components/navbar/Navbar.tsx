71fea71978717719d9271e6d76af9cd17c3fec5b
Evgeniy Klimovch <kliasanik@gmail.com>
2023-05-17 01:17:41 +0300

    active link

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,11 +6,11 @@ import {NavLink} from "react-router-dom";
 export const Navbar = () => {
     return (
         <div className={s.nav}>
-            <div className={s.item}><NavLink to="/profile">Profile</NavLink></div>
-            <div className={`${s.item} ${s.active}`}><NavLink to="/dialogs">Messages</NavLink></div>
-            <div className={s.item}><NavLink to="News">News</NavLink></div>
-            <div className={s.item}><NavLink to="Music">Music</NavLink></div>
-            <div className={s.item}><NavLink to="Settings">Settings</NavLink></div>
+            <div className={s.item}><NavLink to="/profile" className = { navData => navData.isActive ? s.active : s.item }>Profile</NavLink></div>
+            <div className={`${s.item} ${s.active}`}><NavLink to="/dialogs" className = { navData => navData.isActive ? s.active : s.item }>Messages</NavLink></div>
+            <div className={s.item}><NavLink to="News" className = { navData => navData.isActive ? s.active : s.item }>News</NavLink></div>
+            <div className={s.item}><NavLink to="Music" className = { navData => navData.isActive ? s.active : s.item }>Music</NavLink></div>
+            <div className={s.item}><NavLink to="Settings" className = { navData => navData.isActive ? s.active : s.item }>Settings</NavLink></div>
         </div>
     );
 };
