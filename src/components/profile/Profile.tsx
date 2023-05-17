2a2463672025293422a78bc68b2e5833a7c0f14e
Evgeniy Klimovch <kliasanik@gmail.com>
2023-05-17 11:48:37 +0300

    ProfileInfo.tsx

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import s from "./Profile.module.css"
 import {MyPosts} from "./myPosts/MyPosts";
+import {ProfileInfo} from "./profileInfo/ProfileInfo";
 
 export const Profile = () => {
     return (
         <div className={s.content}>
-            <div>
-                <img
-                    src="https://static8.depositphotos.com/1021722/859/i/600/depositphotos_8599315-stock-photo-art-business-power.jpg"/>
-            </div>
-            <div>
-                ava + description
-            </div>
+            <ProfileInfo/>
             <MyPosts/>
         </div>
     );
---
+++ src/components/profile/profileInfo/ProfileInfo.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const ProfileInfo = () => {
+    return (
+        <div>
+            <div>
+                <img
+                    src="https://static8.depositphotos.com/1021722/859/i/600/depositphotos_8599315-stock-photo-art-business-power.jpg"/>
+            </div>
+            <div>
+                ava + description
+            </div>
+        </div>
+    );
+};
+
