30c2e0d5ef8e0baf8fd8569792fe56eeeb00d554
Evgeniy Klimovch <kliasanik@gmail.com>
2023-05-18 19:46:18 +0300

    postData

--- src/components/profile/myPosts/MyPosts.tsx
+++ src/components/profile/myPosts/MyPosts.tsx
@@ -3,6 +3,11 @@ import s from "./MyPosts.module.css";
 import {Post} from "./post/Post";
 
 export const MyPosts = () => {
+    let postData = [
+        {id:1, message:"Hello! This is my first post",likesCounter:6},
+        {id:2, message:"How are you, guys?",likesCounter:12},
+        {id:3, message:"I try to learn js!",likesCounter:88},
+    ]
     return (
         <div className={s.postsBlock}>
             my posts
