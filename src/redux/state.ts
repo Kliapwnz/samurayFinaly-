6ce3fc001da421bd69a25f9db273dd0606733f2d
Evgeniy Klimovch <kliasanik@gmail.com>
2023-05-28 12:30:54 +0300

    state.ts

---
+++ src/redux/state.ts
@@ -0,0 +1,25 @@
+export let state = {
+    profilePage:{
+        posts:[
+            {id: 1, message: "Hello! This is my first post", likesCounter: 6},
+            {id: 2, message: "How are you, guys?", likesCounter: 12},
+            {id: 3, message: "I try to learn js!", likesCounter: 88},
+        ]
+    },
+    dialogsPage:{
+        dialogs:[
+            {id: 1, name: "Evgeniy"},
+            {id: 2, name: "Anastasiya"},
+            {id: 3, name: "Yuri"},
+            {id: 4, name: "Elena"},
+            {id: 5, name: "Kristina"},
+            {id: 6, name: "Kevin"},
+        ],
+        messages:[
+            {id: 1, message: "Hi!"},
+            {id: 2, message: "Do you play football?"},
+            {id: 3, message: "How long?"},
+        ]
+    },
+    sidebar:{}
+}
